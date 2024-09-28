d2406f8a3755e68d030cca0e6f8280a6b4c7a327
Lukasz Myszkowski <lukasz.myszkowski@houseofangular.io>
2024-09-28 19:33:09 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from "@angular/platform-browser";
+
+const ROOT_ELEMENT_TAG = 'app-root';
+
+let rootElement = document.querySelector(ROOT_ELEMENT_TAG);
+
+if (!rootElement) {
+  rootElement = document.createElement(ROOT_ELEMENT_TAG);
+  rootElement.id = 'wsparcio-extension';
+  document.body.appendChild(document.createElement(ROOT_ELEMENT_TAG));
+}
+
+// platformBrowserDynamic()
+//   .bootstrapModule(AppComponent)
+//   .catch((err) => console.error(err));
+bootstrapApplication(AppComponent)
